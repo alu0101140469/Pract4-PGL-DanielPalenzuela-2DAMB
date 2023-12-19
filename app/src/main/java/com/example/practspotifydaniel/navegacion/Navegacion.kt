d40bfc8ddba1bc2a06e5788b745d65e99b1d5b62
Daniel Okumura <daniel-link@hotmail.com>
2023-12-19 23:58:02 +0000

    Añadidas las canciones y arreglo de la interfaz 19/12/2023

--- app/src/main/java/com/example/practspotifydaniel/navegacion/Navegacion.kt
+++ app/src/main/java/com/example/practspotifydaniel/navegacion/Navegacion.kt
@@ -1,21 +1,25 @@
 package com.example.practspotifydaniel.navegacion
 
 import androidx.compose.runtime.Composable
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.practspotifydaniel.pantallas.PantallaReproductor
 import com.example.practspotifydaniel.shared.Rutas
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.practspotifydaniel.shared.ReproductorViewModel
 
 @Composable
 fun GrafoNavegacion(){
 
     val navController = rememberNavController()
 
+    val viewModelScaffold : ReproductorViewModel = viewModel()
+
     NavHost(navController = navController, startDestination = Rutas.Reproductor.ruta){
 
         composable(Rutas.Reproductor.ruta){
-            PantallaReproductor(navController = navController)
+            PantallaReproductor(viewModelScaffold)
         }
 
     }
--- app/src/main/java/com/example/practspotifydaniel/pantallas/Reproductor.kt
+++ app/src/main/java/com/example/practspotifydaniel/pantallas/Reproductor.kt
@@ -1,29 +1,48 @@
 package com.example.practspotifydaniel.pantallas
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowLeft
+import androidx.compose.material.icons.filled.ArrowRight
+import androidx.compose.material.icons.filled.Loop
+import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material.icons.filled.Repeat
+import androidx.compose.material.icons.filled.Shuffle
 import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Shapes
 import androidx.compose.material3.Slider
-import androidx.compose.material3.SliderDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.example.practspotifydaniel.R
-import com.example.practspotifydaniel.shared.Rutas
+import com.example.practspotifydaniel.shared.ReproductorViewModel
 
 
 @Composable
-fun PantallaReproductor() {
+fun PantallaReproductor(viewModelScaffold: ReproductorViewModel) {
 
     Column(
-        modifier = Modifier.padding(32.dp),
+        modifier = Modifier.padding(0.dp),
         verticalArrangement = Arrangement.spacedBy(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
@@ -31,10 +50,18 @@ fun PantallaReproductor() {
             "Now Playing"
         )
         Text(
-            "Angels like you - Miley Cyrus"
+            "cancion - autor"
         )
-        painterResource(id = R.drawable.miley)
-        SliderFuncion()
+        Image(painterResource(id = R.drawable.miley), contentDescription = "",
+            Modifier
+                .fillMaxWidth()
+                .height(200.dp))
+
+        Box(modifier = Modifier.padding(16.dp)) {
+            SliderFuncion()
+        }
+
+
         Row(
             modifier = Modifier.padding(32.dp),
             horizontalArrangement = Arrangement.SpaceBetween
@@ -44,20 +71,37 @@ fun PantallaReproductor() {
         Row(
 
         ) {
-            Button(onClick = { /*TODO*/ }) {
+            Button(
+                onClick = { /*TODO*/ },
+                colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent, contentColor = Color.Black)
+            ) {
+                Icon(imageVector = Icons.Default.Shuffle, contentDescription = "")
 
             }
-            Button(onClick = { /*TODO*/ }) {
-
+            Button(
+                onClick = { /*TODO*/ },
+                colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent, contentColor = Color.Black)
+            ) {
+                Icon(imageVector = Icons.Default.ArrowLeft, contentDescription = "")
             }
-            Button(onClick = { /*TODO*/ }) {
-
+            Button(
+                onClick = { /*TODO*/ },
+                colors = ButtonDefaults.buttonColors(contentColor = Color.Black),
+                shape = CircleShape
+            ) {
+                Icon(imageVector = Icons.Default.PlayArrow, contentDescription = "")
             }
-            Button(onClick = { /*TODO*/ }) {
-
+            Button(
+                onClick = { /*TODO*/ },
+                colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent, contentColor = Color.Black)
+            ) {
+                Icon(imageVector = Icons.Default.ArrowRight, contentDescription = "")
             }
-            Button(onClick = { /*TODO*/ }) {
-
+            Button(
+                onClick = { /*TODO*/ },
+                colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent, contentColor = Color.Black)
+            ) {
+                Icon(imageVector = Icons.Default.Repeat, contentDescription = "")
             }
         }
 
@@ -66,7 +110,9 @@ fun PantallaReproductor() {
 
 @Composable
 fun SliderFuncion() {
-    var sliderPosition by remember { mutableFloatStateOf(0f) }
+    var sliderPosition by remember {
+        mutableFloatStateOf(0f)
+    }
 
     Column {
         Slider(
